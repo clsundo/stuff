ebc8dab160d4e44d433f25b2719422aa1d745711
clsundo <clsundo@protonmail.com>
2021-12-14 10:46:41 -0500

    hello_world.py renamed and modified

--- Tests/delay_hello_world.py
+++ Tests/delay_hello_world.py
@@ -5,7 +5,7 @@ def delay_print(s):
     for c in s:
         sys.stdout.write(c)
         sys.stdout.flush()
-        time.sleep(0.15)
+        time.sleep(0.2)
 
 delay_print("hello world !\n")
 input("appuyez sur entrer pour sortir")
